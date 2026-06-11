14e6b6dd44fd0ec9c816f1d6ecdb1b58a2b0ef27
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-11 11:35:00 +0700

    menambahkan BinaryTree obj

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -128,4 +128,50 @@ public:
             cout << ptr->info << " ";
         }
     }
+};
+
+int main()
+{
+    BinaryTree obj;
+
+    while(true)
+    {
+        cout << "\nMenu" << endl;
+        cout << "1. Imprlement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+        
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            obj.insert(word);
+            break;
+        }
+
+        case '2':
+        {
+            obj.inorder(obj.ROOT);
+            break;
+        }
+
+        case '3':
+        {
+            obj.preorder(obj.ROOT);
+            break;
+        }
+    }
+
 }

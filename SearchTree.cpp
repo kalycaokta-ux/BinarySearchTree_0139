d36f1a9b734af25624c5f27842162e5875f29f76
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-11 09:27:37 +0700

    menambahkan class BinaryTree

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -11,4 +11,13 @@ public:
     Node *rightchild;
 
     // Constructor for the node class
-}
+    Node(string i, Node *l, Node *r)
+    {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+
+    }
+};
+
+class BinaryTree

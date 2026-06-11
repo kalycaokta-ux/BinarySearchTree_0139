c9ea58681434264bb1234b452df3361e339ed314
kalycaokta-ux <kalycaokta@gmail.com>
2026-06-11 11:13:45 +0700

    menambahkan void preorder

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -79,6 +79,21 @@ public:
         }
     }
 
-    void inorder
+    void inorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        
+        if (ptr != NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
 
+    void preorder
 }
